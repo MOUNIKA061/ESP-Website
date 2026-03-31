7cf533293be893238fb0a277967eb444144edcaf
MOUNIKA061 <your-mounika-github-email>
2026-03-31 20:53:12 +0530

    Fix test_invalid_model_does_not_crash: use _call() helper and remove redundant assertion

--- esp/esp/users/tests.py
+++ esp/esp/users/tests.py
@@ -871,13 +871,11 @@ class AjaxAutocompleteViewTest(TestCase):
     def test_invalid_model_does_not_crash(self):
         self.assertTrue(self.client.login(username='staff_autocomplete', password='password'))
 
-        response = self.client.get('/admin/ajax_autocomplete/', {
-            'model_module': 'esp.users.models',
-            'model_name': 'InvalidModel',
-            'ajax_data': 'test',
-        })
+        response = self._call(
+            model_name='InvalidModel',
+            ajax_data='test',
+        )
 
-        self.assertNotEqual(response.status_code, 500)
         self.assertIn(response.status_code, [200, 400])
 
 class StudentInfoFormGradeTest(TestCase):
